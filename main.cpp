bd933617af9c3810631c78e9e216df81c081e398
Shawn Halayka <sjhalayka@gmail.com>
2021-04-21 22:25:27 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
-#pragma comment(lib, "opencv_world346.lib")
+#pragma comment(lib, "opencv_world3413.lib")
 
 #include <iostream>
 #include <vector>
@@ -10,13 +10,12 @@ using namespace std;
 
 
 
-
 void write_histogram(vector<float> input_vec, const char* const filename);
 
 
-int main(void)
+void get_magnitudes(vector<float> &output_vec, const short unsigned int max_iterations)
 {
-	vector<float> input_vec;
+	output_vec.clear();
 
 	size_t res = 100;
 
@@ -37,15 +36,15 @@ int main(void)
 	C.y = 0.5f;
 	C.z = 0.4f;
 	C.w = 0.2f;
-	unsigned short int max_iterations = 8;
-	float threshold = 20.0f;
+
+	float threshold = 4.0;
 
 	string error_string;
 	quaternion_julia_set_equation_parser eqparser;
 	if (false == eqparser.setup("Z = Z^2 + C", error_string, C))
 	{
 		cout << "Equation error: " << error_string << endl;
-		return 0;
+		return;
 	}
 
 	const float x_step_size = (x_grid_max - x_grid_min) / (x_res - 1);
@@ -58,6 +57,8 @@ int main(void)
 
 	for (size_t z = 0; z < z_res; z++, Z.z += z_step_size)
 	{
+		cout << z << " " << z_res << endl;
+
 		Z.x = x_grid_min;
 
 		for (size_t x = 0; x < x_res; x++, Z.x += x_step_size)
@@ -70,18 +71,54 @@ int main(void)
 
 				float mag = eqparser.iterate_magnitude(points, Z, max_iterations, threshold);
 
-				if (1)//mag >= threshold)
+				if (mag < threshold)
 				{
-					double l = 0, d = 0, m = 0;
-					get_path_properties(points, l, d, m);
+					double length = 0, displacement = 0, magnitude = 0;
+					get_path_properties(points, length, displacement, magnitude);
 
-					input_vec.push_back(static_cast<float>(m));
+					output_vec.push_back(static_cast<float>(magnitude));
 				}
-			}	
+			}
+		}
+	}
+}
+
+
+int main(void)
+{
+	vector<vector<float> > input_vec;
+
+	for (short unsigned int i = 0; i <= 8; i++)
+	{
+		vector<float> input;
+
+		get_magnitudes(input, i);
+
+		input_vec.push_back(input);
+	}
+
+	float largest_value = 0;
+
+	for (size_t i = 0; i < input_vec.size(); i++)
+	{
+		for (size_t j = 0; j < input_vec[i].size(); j++)
+		{
+			if (input_vec[i][j] > largest_value)
+				largest_value = input_vec[i][j];
 		}
 	}
 
-	write_histogram(input_vec, "magnitude_all_20.png");
+	for (size_t i = 0; i < input_vec.size(); i++)
+	{
+		input_vec[i].push_back(largest_value);
+
+		ostringstream oss;
+		oss << "magnitude_" << i << ".png";
+
+		write_histogram(input_vec[i], oss.str().c_str());
+	}
+
+
 
 	return 0;
 }
